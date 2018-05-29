079b7a8f7d2bfa03b577e248954bf3bf29cf4dd8
Karsten Ari Agathon <karsten.agathon@traveloka.com>
2018-05-30 06:57:45 +0700

    Fix typo

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -3,7 +3,7 @@ module "elb" {
   product_domain = "BEI"
   service        = "beical"
   lb_name        = "beical-lbint"
-  lb_type        = "classic"
+  lb_type        = "application"
   environment    = "production"
 
   recipients = ["slack-bei", "pagerduty-bei", "bei@traveloka.com"]

0164b1c26a94be2c35e6562ebbd71cc4807b18e6
Karsten Ari Agathon <karsten.agathon@traveloka.com>
2018-06-05 14:51:14 +0700

    Add alert and warning specific recipients. Add additional tags. Add better message template

--- elb-classic.tf
+++ elb-classic.tf
@@ -245,14 +245,18 @@ module "elb_classic_monitor_healthy_host_count" {
   product_domain = "${var.product_domain}"
   service        = "${var.service}"
   environment    = "${var.environment}"
+  tags           = "${var.tags}"
+  timeboard_id   = "${join(",", datadog_timeboard.elb_classic.*.id)}"
 
   name               = "${var.product_domain} - ${var.lb_name} - ${var.environment} - Number of Healthy Hosts is Low"
-  query              = "avg(last_1m):sum:aws.elb.healthy_host_count{name:${var.lb_name}, environment:${var.environment}} by {name, availability-zone} <= ${var.healthy_host_count_thresholds["critical"]}"
+  query              = "avg(last_10m):sum:aws.elb.healthy_host_count{name:${var.lb_name}, environment:${var.environment}} by {name, availability-zone} <= ${var.healthy_host_count_thresholds["critical"]}"
   thresholds         = "${var.healthy_host_count_thresholds}"
   message            = "${var.healthy_host_count_message}"
   escalation_message = "${var.healthy_host_count_escalation_message}"
 
-  recipients = "${var.recipients}"
+  recipients         = "${var.recipients}"
+  alert_recipients   = "${var.alert_recipients}"
+  warning_recipients = "${var.warning_recipients}"
 
   renotify_interval = "${var.renotify_interval}"
   notify_audit      = "${var.notify_audit}"
